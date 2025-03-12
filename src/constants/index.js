41ff98adf60943c19a6394084b7cd7311ef8d6f3
fandrianakis <andrianakisfotios@gmail.com>
2025-03-12 13:16:41 +0200

    Initial commit

---
+++ src/constants/index.js
@@ -0,0 +1,253 @@
+import {
+    mobile,
+    backend,
+    creator,
+    web,
+    javascript,
+    typescript,
+    html,
+    css,
+    reactjs,
+    redux,
+    tailwind,
+    nodejs,
+    mongodb,
+    git,
+    figma,
+    docker,
+    meta,
+    starbucks,
+    tesla,
+    shopify,
+    carrent,
+    jobit,
+    tripguide,
+    threejs,
+  } from "../assets";
+  
+  export const navLinks = [
+    {
+      id: "about",
+      title: "About",
+    },
+    {
+      id: "work",
+      title: "Work",
+    },
+    {
+      id: "contact",
+      title: "Contact",
+    },
+  ];
+  
+  const services = [
+    {
+      title: "Web Developer",
+      icon: web,
+    },
+    {
+      title: "React Developer",
+      icon: mobile,
+    },
+    {
+      title: "Worpress Developer",
+      icon: backend,
+    },
+    {
+      title: "Content Creator",
+      icon: creator,
+    },
+  ];
+  
+  const technologies = [
+    {
+      name: "HTML 5",
+      icon: html,
+    },
+    {
+      name: "CSS 3",
+      icon: css,
+    },
+    {
+      name: "JavaScript",
+      icon: javascript,
+    },
+    {
+      name: "TypeScript",
+      icon: typescript,
+    },
+    {
+      name: "React JS",
+      icon: reactjs,
+    },
+    {
+      name: "Redux Toolkit",
+      icon: redux,
+    },
+    {
+      name: "Tailwind CSS",
+      icon: tailwind,
+    },
+    {
+      name: "Node JS",
+      icon: nodejs,
+    },
+    {
+      name: "MongoDB",
+      icon: mongodb,
+    },
+    {
+      name: "Three JS",
+      icon: threejs,
+    },
+    {
+      name: "git",
+      icon: git,
+    },
+    {
+      name: "figma",
+      icon: figma,
+    },
+    {
+      name: "docker",
+      icon: docker,
+    },
+  ];
+  
+  const experiences = [
+    {
+      title: "Worpress Developer",
+      company_name: "IsHome.gr",
+      icon: starbucks,
+      iconBg: "#383E56",
+      date: "2022-My fist Project",
+      points: [
+        "Developed the company’s website from scratch, ensuring a modern and efficient foundation.",
+        "Ensured cross-browser compatibility and seamless user experience through responsive design.",
+        "Created content based on the specific needs and requirements of the client.",
+        ],
+    },
+    {
+      title: "Worpress Developer",
+      company_name: "IndustrialSafety.gr",
+      icon: tesla,
+      iconBg: "#E6DEDD",
+      date: "2024",
+      points: [
+        "Developed the company’s website from scratch, ensuring a modern and efficient foundation.",
+        "Ensured cross-browser compatibility and seamless user experience through responsive design.",
+        "Created content based on the specific needs and requirements of the client.",
+        ],
+    },
+    {
+      title: "Worpress Developer",
+      company_name: "Karaglanis.gr",
+      icon: shopify,
+      iconBg: "#383E56",
+      date: "2024",
+      points: [
+        "Developed maintained the company’s website, ensuring a modern and efficient foundation.",
+        "Ensured cross-browser compatibility and seamless user experience through responsive design.",
+        "Created content based on the specific needs and requirements of the client.",
+        ],
+    },
+    {
+      title: "Full stack | Worpress Developer",
+      company_name: "Konne.gr",
+      icon: meta,
+      iconBg: "#E6DEDD",
+      date: "Jul 2024 - Present",
+      points: [
+        "Developed maintained the company’s website, ensuring a modern and efficient foundation.",
+        "Ensured cross-browser compatibility and seamless user experience through responsive design.",
+        "Created content based on the specific needs and requirements of the client.",
+        ],
+    },
+  ];
+  
+  const testimonials = [
+    {
+      testimonial:
+        "I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
+      name: "Sara Lee",
+      designation: "CFO",
+      company: "Acme Co",
+      image: "https://randomuser.me/api/portraits/women/4.jpg",
+    },
+    {
+      testimonial:
+        "I've never met a web developer who truly cares about their clients' success like Rick does.",
+      name: "Chris Brown",
+      designation: "COO",
+      company: "DEF Corp",
+      image: "https://randomuser.me/api/portraits/men/5.jpg",
+    },
+    {
+      testimonial:
+        "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
+      name: "Lisa Wang",
+      designation: "CTO",
+      company: "456 Enterprises",
+      image: "https://randomuser.me/api/portraits/women/6.jpg",
+    },
+  ];
+  
+  const projects = [
+    {
+      name: " Premium Olive Oil Producer",
+      description:
+        "Offers ultra-premium extra virgin olive oil from Pylos, Messinia, Greece, emphasizing traditional cultivation and high polyphenol content for health benefits.",
+      tags: [
+        {
+          name: "WorPress",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "Elementor",
+          color: "pink-text-gradient",
+        },
+      ],
+      image: carrent,
+      source_code_link: "https://karaglanis.gr/",
+    },
+    {
+      name: " Occupational Safety Services",
+      description:
+        "Specializes in risk management and environmental protection since 1987, offering services like safety engineering, emergency preparedness, and occupational health training.",
+      tags: [
+        {
+          name: "WordPress",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "Elementor",
+          color: "pink-text-gradient",
+        },
+      ],
+      image: jobit,
+      source_code_link: "https://industrialsafety.gr/",
+    },
+    {
+      name: "Maritime Community Platform",
+      description:
+        "A comprehensive platform offering maritime professionals information, services, job listings, and educational resources to support their careers in the shipping industry. ",
+      tags: [
+        {
+          name: "WordPress",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "JetEngine",
+          color: "green-text-gradient",
+        },
+        {
+          name: "JavaScript",
+          color: "pink-text-gradient",
+        },
+      ],
+      image: tripguide,
+      source_code_link: "https://konne.gr/",
+    },
+  ];
+  
+  export { services, technologies, experiences, testimonials, projects };
